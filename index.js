acaf454608e713e3b8e194d5144321575fe31bb0
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2022-08-07 01:09:48 +0600

    connectey

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.get("/", async (req, res)=> {
-    res.send('assignment server is running!')
+    res.send('assignment server is running!!!')
 });
 
 app.listen(port, ()=>{
