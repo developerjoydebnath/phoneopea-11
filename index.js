f526a0e439bd081fb1e8df7ecf0c5d27dc178984
Joy Debnath <96875662+developerjoydebnath@users.noreply.github.com>
2022-09-10 03:52:57 +0600

    random data load done

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ app.use(cors());
 app.use(express.json());
 
 
+const verifyJWT = (req, res, next) => {
+    // console.log(req?.headers?.authorization)
+    const authHeader = req?.headers?.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1]
+    // console.log(token)
+    jwt.verify(token, process.env.SECRET_PRIVET_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
+
+
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xwuxkhj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
@@ -19,22 +39,40 @@ async function run() {
         const phonesCollection = client.db("warehouse").collection("phones");
 
         // load all data
-        app.get("/phones", async(req, res)=> {
+        app.get("/allPhone", async (req, res)=> {
+            const query = {};
+            const cursor = phonesCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // load all data for pagination
+        app.get("/phones", async (req, res) => {
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
             const query = {};
             const phones = phonesCollection.find(query);
-            const result = await phones.toArray();
+
+            let result;
+            if(page || size){
+                result = await phones.skip(page * size).limit(size).toArray();
+            }
+            else{
+                result = await phones.toArray();
+            }
             res.send(result);
         });
 
         // load short data from database
         app.get("/shortPhones", async (req, res) => {
-            const shortResult = phonesCollection.find({}).limit(6);
+            // const shortResult = phonesCollection.find({}).limit(6);
+            const shortResult = phonesCollection.aggregate([{ $sample: { size: 6 } }])
             const result = await shortResult.toArray();
             res.send(result);
         })
 
         // load search data by name
-        app.get("/searchResult", async(req, res)=> {
+        app.get("/searchResult", async (req, res) => {
             const query = req.query.search;
             const queryAll = {}
             const cursor = phonesCollection.find(queryAll);
@@ -44,22 +82,22 @@ async function run() {
         })
 
         // load a single data by id
-        app.get("/phoneDetails", async (req, res)=> {
+        app.get("/phoneDetails", async (req, res) => {
             const id = req.query.id;
-            const query = {_id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await phonesCollection.findOne(query);
             res.send(result)
         })
 
         // update restock quantity of a phone 
-        app.put("/restockItem", async ( req, res) => {
+        app.put("/restockItem", async (req, res) => {
             const id = req.query.id;
             const updateUser = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true }
             const updateDoc = {
                 $set: {
-                    quantity : updateUser.quantity
+                    quantity: updateUser.quantity
                 }
             };
             const result = await phonesCollection.updateOne(filter, updateDoc, options);
@@ -69,14 +107,14 @@ async function run() {
 
 
         // update delivered quantity of a phone 
-        app.put("/deliveredItem", async ( req, res) => {
+        app.put("/deliveredItem", async (req, res) => {
             const id = req.query.id;
             const updateUser = req.body;
             const filter = { _id: ObjectId(id) };
             const options = { upsert: true }
             const updateDoc = {
                 $set: {
-                    quantity : updateUser.quantity
+                    quantity: updateUser.quantity
                 }
             };
             const result = await phonesCollection.updateOne(filter, updateDoc, options);
@@ -92,21 +130,41 @@ async function run() {
         })
 
         //find items by user email address
-        app.get("/myInventory", async (req,  res) => {
+        app.get("/myInventory", verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const userEmail = req.query.email;
-            const query = {email : { $in : [userEmail]}};
-            const cursor = phonesCollection.find(query);
-            const result = await cursor.toArray();
-            res.send(result);
+            if (decodedEmail === userEmail) {
+                const query = { email: { $in: [userEmail] } };
+                const cursor = phonesCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result);
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden access' })
+            }
         })
 
         // delete item from database
-        app.delete("/deleteItem", async(req, res)=> {
+        app.delete("/deleteItem", async (req, res) => {
             const id = req.query.id;
             console.log(id)
-            const query = { _id : ObjectId(id)};
+            const query = { _id: ObjectId(id) };
             const result = await phonesCollection.deleteOne(query);
             res.send(result);
+        })
+
+        app.post("/login", async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.SECRET_PRIVET_TOKEN, {
+                expiresIn: '1d'
+            })
+            res.send({ accessToken });
+        })
+
+        // product count 
+        app.get("/productCount", async(req, res)=> {
+            const count = await phonesCollection.estimatedDocumentCount();
+            res.send({count});
         })
 
     }
